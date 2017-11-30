664d5afeac3b936c3a665a11b0f80815f248b239
BlackHe <1124681011@qq.com>
2017-12-01 00:05:09 +0800

    新建spring boot项目骨架，起步依赖包含 web,jpa,lombok,themleaf等

---
+++ src/main/java/com/peony/readinglist/ReadinglistApplication.java
@@ -0,0 +1,12 @@
+package com.peony.readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadinglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadinglistApplication.class, args);
+	}
+}
