5477767787e743b1ef3646a5ca5c697b06a65c16
BlackHe <1124681011@qq.com>
2017-12-01 12:51:39 +0800

    1.配置文件改为连接本地数据库 2.添加测试controller 3.配置文件给为yml文件

--- src/main/java/com/peony/readinglist/ReadinglistApplication.java
+++ src/main/java/com/peony/readinglist/ReadinglistApplication.java
@@ -3,6 +3,8 @@ package com.peony.readinglist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileInputStream;
+
 @SpringBootApplication
 public class ReadinglistApplication {
 
--- src/main/java/com/peony/readinglist/controller/HelloController.java
+++ src/main/java/com/peony/readinglist/controller/HelloController.java
@@ -1,10 +1,20 @@
 package com.peony.readinglist.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Create by Intrllij IDEA
  * User:black
  * Date:
  * Time:
  */
+@Controller
 public class HelloController {
+
+    @GetMapping(value = "/")
+    public String hello(){
+        return "hello";
+    }
 }
