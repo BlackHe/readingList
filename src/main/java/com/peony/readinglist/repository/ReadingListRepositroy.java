6bbd9068cd7a4626e02e1f3f41999e53a194280f
BlackHe <1124681011@qq.com>
2017-12-02 16:48:46 +0800

    新增阅读列表数据访问实体，基础JpaRepository

---
+++ src/main/java/com/peony/readinglist/repository/ReadingListRepositroy.java
@@ -0,0 +1,15 @@
+package com.peony.readinglist.repository;
+
+import com.peony.readinglist.entity.Book;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Create by Intellij IDEA
+ * User:black
+ * description:
+ */
+public interface ReadingListRepositroy extends JpaRepository<Book,Integer>{
+
+    List<Book> findByReader(String reader);
+}
